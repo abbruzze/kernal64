096a1d89871c3e0c6a54694bc0199a6e2f143bfb
ealeame <ealeame@IT00040215.ericsson.se>
2015-02-17 18:17:55 +0100

    Fixed years

--- Kernal64/src/ucesoft/c64/util/AboutCanvas.scala
+++ Kernal64/src/ucesoft/c64/util/AboutCanvas.scala
@@ -17,7 +17,7 @@ class AboutCanvas(charRom:Memory,version:String) extends CBMCanvas(charRom) {
   newLine
   add(center("BY ALESSANDRO ABBRUZZETTI",WIDTH)).newLine
   newLine
-  add(center("2013-2014",WIDTH)).newLine
+  add(center("2013-2014-2015",WIDTH)).newLine
   newLine
   yellow
   add(center("VISIT",WIDTH)).newLine
