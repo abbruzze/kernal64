f61fd80f551f98a7c31f61469224a2c214645ca1
ealeame <ealeame@IT00040215.ericsson.se>
2015-03-27 17:19:31 +0100

    Fixed installation package

--- Kernal64/build/kernal64.bat
+++ Kernal64/build/kernal64.bat
@@ -1,5 +1,5 @@
 @echo off
 set HOME=%~dp0
 set LIB=%HOME%lib
-set CP=%LIB%\kernal64.jar;%LIB%\jinput.jar;%LIB%\scala-library.jar
+set CP=%LIB%\kernal64.jar;%LIB%\jinput.jar;%LIB%\scala-library.jar;%LIB%\scala-parser-combinators_2.11-1.0.3.jar;%LIB%\commons-net-3.3.jar;%LIB%\dropbox-core-sdk-1.7.7.jar;%LIB%\jackson-core-2.2.4.jar
 start javaw -server -Xms64M -Xmx64M -cp %CP% -Djava.library.path=%LIB% ucesoft.c64.C64
--- Kernal64/build/kernal64.sh
+++ Kernal64/build/kernal64.sh
@@ -3,7 +3,7 @@
 HOME=`dirname "$0"`
 HOME=`cd "$HOME"; pwd -P`
 LIB=$HOME/lib
-CP=$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar
+CP=$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar:$LIB/scala-parser-combinators_2.11-1.0.3.jar:$LIB/commons-net-3.3.jar:$LIB/dropbox-core-sdk-1.7.7.jar:$LIB/jackson-core-2.2.4.jar
 if [ ! -x $JAVA_HOME/bin/java ]; then
         JAVA=java
 else
