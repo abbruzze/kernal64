54274a9b9f29296d5dd137ad4989e22da54aef7e
Alessandro Abbruzzetti <abbruzze@gmail.com>
2015-08-30 19:09:59 +0200

    Fixed IRQ 7th bit handling Removed scheduling event for this VIA. Use clocking from C1541

--- Kernal64/src/ucesoft/c64/peripheral/drive/VIA.scala
+++ Kernal64/src/ucesoft/c64/peripheral/drive/VIA.scala
@@ -50,11 +50,10 @@ abstract class VIA(val name:String,
   private[this] val PA_LATCH_ENABLED = 0x01
   private[this] val PB_LATCH_ENABLED = 0x02
   
-  private[this] val clock = Clock.systemClock
   protected var active = true
   
   def init {
-    clock.schedule(new ClockEvent(name,clock.nextCycles,update _))
+    active = true
   }
   
   def reset = {
@@ -64,26 +63,25 @@ abstract class VIA(val name:String,
     
   def setActive(active:Boolean) {
     if (!this.active && active) {
-      if (Thread.currentThread != clock) {
-        clock.pause
-        init
-        clock.play
-      }
-      else init
+      init
     }
     this.active = active
   }
   
   protected def irq_set(irq:Int) {
-    regs(IFR) |= irq | 0x80	// enable irq bit + 7th bit
+    regs(IFR) |= irq //| 0x80	// enable irq bit + 7th bit
     checkIRQ
   }
   protected def irq_clr(irq:Int) {
     regs(IFR) &= ~irq & 0x7F
-    if ((regs(IFR) & 0x7F) == 0) regs(IFR) = 0 // if no more irq are set clear 7th bit
+    //if ((regs(IFR) & 0x7F) == 0) regs(IFR) = 0 // if no more irq are set clear 7th bit
     checkIRQ
   }
-  @inline protected def checkIRQ = irqAction((regs(IFR) & regs(IER)) > 0)  
+  @inline protected def checkIRQ = {
+    val irq = (regs(IFR) & regs(IER)) > 0
+    if (irq) regs(IFR) |= 0x80 else regs(IFR) &= 0x7F
+    irqAction(irq)  
+  }
   
   @inline protected def is_set(reg:Int,bits:Int) = (regs(reg) & bits) > 0
   
@@ -104,9 +102,9 @@ abstract class VIA(val name:String,
     case T1LC =>
       irq_clr(IRQ_TIMER_1)
       regs(T1LC)
-	case T2LC =>
-	  irq_clr(IRQ_TIMER_2)
-	  regs(T2LC)
+	  case T2LC =>
+  	  irq_clr(IRQ_TIMER_2)
+  	  regs(T2LC)
     case ofs => regs(ofs)
   }
   
@@ -163,16 +161,18 @@ abstract class VIA(val name:String,
       Log.debug(s"${name} writing reg ${ofs} => ${Integer.toBinaryString(value)}")
   }
   
-  protected def update(cycles:Long) {
-    updateT1
-    updateT2
-    if (active) clock.schedule(new ClockEvent(name,cycles + UPDATE_CLOCKS,update _))
+  def clock(cycles:Long) {
+    if (active) {
+      updateT1
+      updateT2
+    }
+    //if (active) clock.schedule(new ClockEvent(name,cycles + UPDATE_CLOCKS,update _))
   }
   
   private def updateT1 {
     var counter = regs(T1LC) | regs(T1HC) << 8
     //Log.debug(s"T1[${name}] counter = ${counter}")
-    counter -= UPDATE_CLOCKS
+    counter -= 1//UPDATE_CLOCKS
     if (counter <= 0) {
       if (is_set(ACR,0x40)) { // free running mode
         if (is_set(ACR,0x80)) PB7 = if (PB7 == 0x00) 0x80 else 0x00
@@ -192,7 +192,7 @@ abstract class VIA(val name:String,
   private def updateT2 {
     if (!is_set(ACR,0x20)) {	// DO NOT count pulses on PB6
       var counter = regs(T2LC) | regs(T2HC) << 8
-      counter -= UPDATE_CLOCKS
+      counter -= 1//UPDATE_CLOCKS
       if (counter <= 0) irq_set(IRQ_TIMER_2)
       regs(T2LC) = counter & 0xFF
       regs(T2HC) = (counter >> 8) & 0xFF
