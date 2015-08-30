c4d66400d01cb911c92be7cee08f9489d859fffe
Alessandro Abbruzzetti <abbruzze@gmail.com>
2015-08-30 19:12:13 +0200

    Added support for Floppy trait Modified byte read management: now every bit is handled; the SYNC condition is checked against a shift register

--- Kernal64/src/ucesoft/c64/peripheral/drive/VIADiskControl.scala
+++ Kernal64/src/ucesoft/c64/peripheral/drive/VIADiskControl.scala
@@ -2,7 +2,6 @@ package ucesoft.c64.peripheral.drive
 
 import ucesoft.c64.ChipID
 import ucesoft.c64.Clock
-import ucesoft.c64.formats.D64
 import ucesoft.c64.cpu.CPU6510
 import ucesoft.c64.ClockEvent
 
@@ -14,8 +13,6 @@ class VIADiskControl(var cpu: CPU6510,
   private[this] val WRITE_PROTECT_SENSE_WAIT = 3 * 400000L
   private[this] val REMOVING_DISK_WAIT = 500000L
   private[this] val SYNC_DETECTION_LINE = 0x80
-  private[this] val WAIT_CYCLES_INTER_SECTORS = 1000
-  private[this] var TOTAL_TRACKS = 0
   private[this] var isWriting = false
   private[this] var isDiskChanged = true
   private[this] var isDiskChanging = false
@@ -26,16 +23,11 @@ class VIADiskControl(var cpu: CPU6510,
   private[this] var byteReady = false
   private[this] var trackSteps = 2
   private[this] var track = 1
-  private[this] var sector = 0
-  private[this] var lastSync = false
-  private[this] var sectorsPerCurrentTrack = D64.TRACK_ALLOCATION(track)
-  private[this] var gcrSector: Array[Int] = null
-  private[this] var gcrIndex,gcrIndexFromToWrite = 0
-  private[this] var sectorModified = false
-  private[this] var d64: Option[D64] = None
+  private[this] var floppy : Floppy = EmptyFloppy
   private[this] var currentFilename = ""
+  
   /**
-   * Get the rotation cycles needed for read a byte for the current track.
+   * Get the rotation cycles needed for read a byte for the given zone.
    *
    * Zone	Tracks	Clock rate
    * ----------------------------
@@ -44,29 +36,45 @@ class VIADiskControl(var cpu: CPU6510,
    * 3		25-30	266.667 bit/s
    * 4		31-35	250.000 bit/s
    */
-  private[this] val rotationCyclesForByte: Int => Double = {
-    val C1541_CLOCK_HZ = 1000000
-    val zones = Array.ofDim[Double](4)
-    zones(0) = (307692.0 / C1541_CLOCK_HZ)
-    zones(1) = (285714.0 / C1541_CLOCK_HZ)
-    zones(2) = (266667.0 / C1541_CLOCK_HZ)
-    zones(3) = (250000.0 / C1541_CLOCK_HZ)
-    t: Int => {
-      if (t <= 17) zones(0)
-      else if (t <= 24) zones(1)
-      else if (t <= 30) zones(2)
-      else zones(3)
-    }
+  private[this] val C1541_CLOCK_HZ = 1000000
+  private[this] val rotationCyclesForBit = Array[Double](
+      250000.0 / C1541_CLOCK_HZ,  // zone 4
+      266667.0 / C1541_CLOCK_HZ,  // zone 3
+      285714.0 / C1541_CLOCK_HZ,  // zone 2
+      307692.0 / C1541_CLOCK_HZ   // zone 1 
+  )
+    
+  private[this] var speedZone = 3
+  private[this] var bitCycleWait = rotationCyclesForBit(speedZone)
+  private[this] var rotationCycleCounter = 0.0  
+  private[this] var bitCounter = 0
+  private[this] var last10Bits,lastRead = 0
+  
+  override def reset {
+    super.reset
+    isWriting = false
+    isDiskChanged = true
+    isDiskChanging = false
+    diskChangedAtClockCycle = 0L
+    motorOn = false
+    canSetByteReady = false
+    byteReady = false
+    var trackSteps = 2
+    var track = 1
+    currentFilename = ""
+    bitCounter = 0
+    speedZone = 3
+    bitCycleWait = rotationCyclesForBit(speedZone)
+    rotationCycleCounter = 0.0
+    last10Bits = 0
+    lastRead = 0
   }
-  private[this] var byteCycleWait = rotationCyclesForByte(1)
-  private[this] var rotationCycleCounter = 0.0
   
   override def getProperties = {
     properties.setProperty("Motor on",motorOn.toString)
     properties.setProperty("Writing",isWriting.toString)
     properties.setProperty("Track","%2d".format(track))
-    properties.setProperty("Sector","%2d".format(sector))
-    properties.setProperty("Disk",if (d64.isDefined) d64.get.file else "")
+    properties.setProperty("Disk",floppy.toString)
     properties.setProperty("Current filename",currentFilename)
     super.getProperties
   } 
@@ -77,10 +85,11 @@ class VIADiskControl(var cpu: CPU6510,
   
   def setReadOnly(readOnly:Boolean) = isReadOnly = readOnly
 
-  def setDriveReader(driveReader: D64) {
-    d64 = Some(driveReader)
-    TOTAL_TRACKS = d64.get.TOTAL_TRACKS
-    gcrSector = driveReader.gcrImageOf(track, sector)
+  def setDriveReader(driveReader:Floppy) {
+    floppy = driveReader    
+    // reset the last track
+    floppy.changeTrack(trackSteps)
+    floppy.setTrackChangeListener(updateTrackSectorLabelProgress _)
     isDiskChanged = true
     isDiskChanging = true
     Clock.systemClock.schedule(new ClockEvent("DiskRemoving",Clock.systemClock.currentCycles + WRITE_PROTECT_SENSE_WAIT, cycles => {
@@ -94,32 +103,25 @@ class VIADiskControl(var cpu: CPU6510,
   
   def setCurrentFilename(fn:String) = currentFilename = fn
   def formatDisk = {
-    d64 match {
-      case None => false
-      case Some(disk) =>
-        try {
-          println("Formatting '" + currentFilename + "'")
-          disk.format(currentFilename)
-          true
-        }
-        catch {
-          case t:IllegalArgumentException =>
-            t.printStackTrace
-            false
-        }
+    try {
+      println("Formatting '" + currentFilename + "'")
+      floppy.format(currentFilename)
+      true
+    }
+    catch {
+      case t:IllegalArgumentException =>
+        t.printStackTrace
+        false
     }
   }
   
   def isMotorOn = motorOn
 
-  @inline private def isSync = d64 match {
-    case None => false
-    case Some(d) => motorOn && gcrSector(gcrIndex) == 0xFF// && gcrIndex + 1 < gcrSector.length && gcrSector(gcrIndex + 1) != 0xFF
-  }
+  @inline private def isSync = !isWriting && motorOn && last10Bits == 0x3FF  
 
   override def read(address: Int, chipID: ChipID.ID) = address - startAddress match {
     case PB =>
-      val wps = if (isDiskChanging) isDiskChanged else isReadOnly
+      val wps = if (isDiskChanging) isDiskChanged else isReadOnly | floppy.isReadOnly
       if (isDiskChanging && isDiskChanged && Clock.systemClock.currentCycles - diskChangedAtClockCycle > WRITE_PROTECT_SENSE_WAIT) {
         isDiskChanged = false
         isDiskChanging = false
@@ -129,10 +131,9 @@ class VIADiskControl(var cpu: CPU6510,
         (if (wps) 0x00 else WRITE_PROTECT_SENSE)
     case PA|PA2 =>
       super.read(address, chipID)
-      val headByte = if (gcrSector == null) 0 else gcrSector(gcrIndex)
-      updateTrackSectorLabelProgress
+      floppy.notifyTrackSectorChangeListener
       //println(s"READ ${Integer.toHexString(headByte)} ${track}/${sector} gcrIndex=${gcrIndex}")
-      headByte
+      lastRead
     case ofs => super.read(address, chipID)
   }
 
@@ -142,7 +143,7 @@ class VIADiskControl(var cpu: CPU6510,
         canSetByteReady = (value & 2) > 0
         if ((value & 0x20) == 0) {
           isWriting = true
-          gcrIndexFromToWrite = gcrIndex + 1
+          floppy.prepareToWrite
         }
         else isWriting = false
       case PB =>
@@ -157,11 +158,18 @@ class VIADiskControl(var cpu: CPU6510,
             if (lastMotorOn && !motorOn) currentFilename = ""
           }
         }
+        val newSpeedZone = (value & 0xFF) >> 5 & 0x3
+        if (newSpeedZone != speedZone) {
+          speedZone = newSpeedZone
+          bitCycleWait = rotationCyclesForBit(speedZone)
+          rotationCycleCounter = 0.0
+        }
         // check the head step indication
         val oldStepHead = regs(PB) & 0x03
         val stepHead = value & 0x03
         if (motorOn && oldStepHead == ((stepHead + 1) & 0x03) && track > 1) moveHead(moveOut = true)
-        else if (motorOn && oldStepHead == ((stepHead - 1) & 0x03) && track < TOTAL_TRACKS) moveHead(moveOut = false)
+        else 
+        if (motorOn && oldStepHead == ((stepHead - 1) & 0x03) && track < floppy.totalTracks) moveHead(moveOut = false)
       case _ =>
     }
     // write on register
@@ -170,63 +178,53 @@ class VIADiskControl(var cpu: CPU6510,
 
   private def moveHead(moveOut: Boolean) {
     if (moveOut) trackSteps -= 1 else trackSteps += 1
-    val isOnTrack = (trackSteps & 1) == 0
-    if (isOnTrack) {
-      track = trackSteps >> 1/// 2
-      sectorsPerCurrentTrack = D64.TRACK_ALLOCATION(track)
-      sector = 0            
-      byteCycleWait = rotationCyclesForByte(track)
-      rotationCycleCounter = 0.0
-      if (d64.isDefined) gcrSector = d64.get.gcrImageOf(track, sector)
-      //println(s"Moved on track ${track}")
+    floppy.changeTrack(trackSteps)
+    track = floppy.currentTrack
+  }
+  
+  private def updateTrackSectorLabelProgress(track:Int,halfTrack:Boolean,sector:Option[Int]) {
+    if (ledListener != null) {      
+      sector match {
+        case None =>
+          val trackFormat = if (halfTrack) "%02d." else "%02d "
+          ledListener.beginLoadingOf(s"%s $trackFormat".format(currentFilename,track),true)
+        case Some(sec) =>
+          ledListener.beginLoadingOf("%s %02d/%02d".format(currentFilename,track,sec),true)
+      }
     }
   }
   
-  private def updateTrackSectorLabelProgress {
-    if (ledListener != null) ledListener.beginLoadingOf("%s %02d/%02d".format(currentFilename,track,sector),true)
-  }
-  
-  private def rotateDisk : Int = {
-    if (gcrSector != null) {
-      rotationCycleCounter += byteCycleWait
-      if (rotationCycleCounter < 8) return 0
-      rotationCycleCounter -= 8
-      
-      if (isWriting && gcrIndex >= gcrIndexFromToWrite) {    
-        gcrSector(gcrIndex) = regs(PA)
-        sectorModified = true
-      }
-      gcrIndex += 1
-      if (gcrIndex >= gcrSector.length) { // end of current sector
-        if (sectorModified) {
-          sectorModified = false
-          d64.get.writeGCRSector(track,sector,gcrSector)
-        }
-        gcrIndex = 0
-        sector = (sector + 1) % sectorsPerCurrentTrack        
-        gcrSector = d64.get.gcrImageOf(track, sector)
-      }
+  private def rotateDisk {
+    rotationCycleCounter += bitCycleWait
+    if (rotationCycleCounter >= 1) {
+      bitCounter += 1
+      rotationCycleCounter -= 1
+      val bit = floppy.nextBit
+      last10Bits = ((last10Bits << 1) | bit) & 0x3FF
+      if (!isWriting && last10Bits == 0x3FF) bitCounter = 0
+    }
+    if (bitCounter < 8) return
+    bitCounter = 0
+    
+    if (isWriting && floppy.canWrite) {    
+      floppy.writeByte(regs(PA))
+    }
 
-      if (isWriting || !isSync || !lastSync) {
-        byteReady = true
-        irq_set(IRQ_CA1)
-//        cpu.setOverflowFlag // byte ready
-      }
-      lastSync = isSync
-      0
-    } 
-    else 0
+    if (isWriting || !isSync) {
+      byteReady = true
+      lastRead = last10Bits & 0xFF
+      irq_set(IRQ_CA1)
+    }
   }
 
-  def clock(cycles: Long) = {
+  override def clock(cycles: Long) {
+    super.clock(cycles)
     if (motorOn) {
-      val ret = rotateDisk
+      rotateDisk
       if (byteReady && canSetByteReady) {
         cpu.setOverflowFlag
         byteReady = false
       }
-      ret 
     }
-    else 0
   }
 }
