419308c70ed80cc85f5a4db3835f6e226fbbdc01
ealeame <ealeame@IT00040215.ericsson.se>
2015-09-07 16:36:10 +0200

    Adapted for Floppy's modifications Now it's writable and formattable (and it works!)

--- Kernal64/src/ucesoft/c64/formats/G64.scala
+++ Kernal64/src/ucesoft/c64/formats/G64.scala
@@ -2,70 +2,74 @@ package ucesoft.c64.formats
 
 import ucesoft.c64.peripheral.drive.Floppy
 import java.io.RandomAccessFile
+import java.io.File
+import java.nio.file._
 
-object G64 extends App {
-  println(new G64(args(0)))
+object G64 {
+  def makeEmptyDisk(file:String) {
+    val emptyDisk = getClass.getResourceAsStream("/resources/emptyDisk.g64")
+    if (emptyDisk == null) throw new IllegalArgumentException
+    Files.copy(emptyDisk,new File(file).toPath,StandardCopyOption.REPLACE_EXISTING)
+  }
 }
 
 
 class G64(file:String) extends Floppy {
   private[this] var tracks : Array[Array[Int]] = _
+  private[this] var trackOffsets : Array[Int] = _
   private[this] var speedZones : Array[Int] = _
   private[this] var trackIndex = 0
   private[this] var track = 0
   private[this] var bit = 1
+  private[this] val g64 = new RandomAccessFile(file, "rw")
+  private[this] var trackIndexModified = false
   
   loadTracks
   
-  val isReadOnly = true
+  val isReadOnly = false
+  val isFormattable = true
   lazy val totalTracks = tracks.length
   
   private[this] var trackChangeListener : Floppy#TrackListener = null
   
-  private def loadTracks {
-    val g64 = new RandomAccessFile(file, "r")
-    try {
-      val header = Array.ofDim[Byte](0x0C)
-      g64.readFully(header)
-      val magic = "GCR-1541".toArray
-      for(i <- 0 to 7) if (header(i) != magic(i)) throw new IllegalArgumentException("Bad signature")
-      tracks = Array.ofDim[Array[Int]](header(0x09))
-      speedZones = Array.ofDim[Int](tracks.length)
-      
-      val trackOffsets = Array.ofDim[Int](tracks.length)
-      // read track offset
-      for(i <- 0 until trackOffsets.length) {      
-        val offset = g64.read | 
-                     g64.read << 8 | 
-                     g64.read << 16 |
-                     g64.read << 24
-        //println(s"Track $i offset = ${Integer.toHexString(offset)}")
-        trackOffsets(i) = offset
-      }
-      // read speed zones
-      for(i <- 0 until tracks.length) {        
-        speedZones(i) = g64.read
-        if (speedZones(i) > 3) {
-          speedZones(i) = defaultZoneFor(i + 1)
-          println(s"Track ${i + 1} has a custom speed zone: not supported. Set as ${speedZones(i)}")
-        }
-        g64.skipBytes(3)
-      }
-      // read track data
-      for(i <- 0 until trackOffsets.length) {
-        if (trackOffsets(i) == 0) tracks(i) = Array.fill[Int](1)(0)
-        else {
-          g64.seek(trackOffsets(i))
-          val trackLen = g64.read | g64.read << 8
-          //println(s"Track $i len=${Integer.toHexString(trackLen)}")
-          tracks(i) = Array.ofDim[Int](trackLen)
-          for(d <- 0 until trackLen) tracks(i)(d) = g64.read
-        }
-      }   
+  private def loadTracks {    
+    val header = Array.ofDim[Byte](0x0C)
+    g64.readFully(header)
+    val magic = "GCR-1541".toArray
+    for(i <- 0 to 7) if (header(i) != magic(i)) throw new IllegalArgumentException("Bad signature")
+    tracks = Array.ofDim[Array[Int]](header(0x09))
+    speedZones = Array.ofDim[Int](tracks.length)
+    
+    trackOffsets = Array.ofDim[Int](tracks.length)
+    // read track offset
+    for(i <- 0 until trackOffsets.length) {      
+      val offset = g64.read | 
+                   g64.read << 8 | 
+                   g64.read << 16 |
+                   g64.read << 24
+      //println(s"Track $i offset = ${Integer.toHexString(offset)}")
+      trackOffsets(i) = offset
     }
-    finally {
-      g64.close
+    // read speed zones
+    for(i <- 0 until tracks.length) {        
+      speedZones(i) = g64.read
+      if (speedZones(i) > 3) {
+        speedZones(i) = defaultZoneFor(i + 1)
+        //println(s"Track ${i + 1} has a custom speed zone: not supported. Set as ${speedZones(i)}")
+      }
+      g64.skipBytes(3)
     }
+    // read track data
+    for(i <- 0 until trackOffsets.length) {
+      if (trackOffsets(i) == 0) tracks(i) = Array.fill[Int](1)(0)
+      else {
+        g64.seek(trackOffsets(i))
+        val trackLen = g64.read | g64.read << 8
+        //println(s"Track $i len=${Integer.toHexString(trackLen)}")
+        tracks(i) = Array.ofDim[Int](trackLen)
+        for(d <- 0 until trackLen) tracks(i)(d) = g64.read
+      }
+    }   
   }
   
   def reset {
@@ -79,12 +83,20 @@ class G64(file:String) extends Floppy {
     if (bit == 8) rotate else bit += 1
     b
   }
-  def writeByte(value:Int) {}
-  def prepareToWrite {}
-  def canWrite = false
+  def writeNextBit(value:Boolean) {
+    trackIndexModified = true
+    val mask = 1 << (8 - bit)
+    if (value) tracks(track)(trackIndex) |= mask else tracks(track)(trackIndex) &= ~mask
+    if (bit == 8) rotate else bit += 1
+  }
   
   @inline private def rotate {
     bit = 1
+    if (trackIndexModified) {
+      trackIndexModified = false
+      g64.seek(trackOffsets(track) + trackIndex + 2)
+      g64.write(tracks(track)(trackIndex))
+    }
     trackIndex = (trackIndex + 1) % tracks(track).length
   }
   
@@ -101,7 +113,7 @@ class G64(file:String) extends Floppy {
   
   def format(diskName:String) {}
   
-  def close {}
+  def close = g64.close
   
   override def toString = s"G64 $file total tracks=$totalTracks"
 }
