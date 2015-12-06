891670bcf6d2330e0ccaec4838373ce7f427863c
Alessandro Abbruzzetti <abbruzze@gmail.com>
2015-12-06 11:18:22 +0100

    New version 1.2.3

--- Kernal64/src/ucesoft/c64/C64.scala
+++ Kernal64/src/ucesoft/c64/C64.scala
@@ -66,7 +66,7 @@ class C64 extends C64Component with ActionListener with TraceListener {
   val componentID = "Commodore 64"
   val componentType = C64ComponentType.INTERNAL
   
-  private[this] val VERSION = "1.2.2"
+  private[this] val VERSION = "1.2.3"
   private[this] val CONFIGURATION_FILENAME = "C64.config"
   private[this] val CONFIGURATION_LASTDISKDIR = "lastDiskDirectory"
   private[this] val CONFIGURATION_FRAME_XY = "frame.xy"  
