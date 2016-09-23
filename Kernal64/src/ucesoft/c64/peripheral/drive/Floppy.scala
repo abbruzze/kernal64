415709f849ef022fe160c0f4141b0e8b7e9faa7a
ealeame <ealeame@IT00040215.ericsson.se>
2016-09-23 18:18:28 +0200

    State saving modifications

--- Kernal64/src/ucesoft/c64/peripheral/drive/Floppy.scala
+++ Kernal64/src/ucesoft/c64/peripheral/drive/Floppy.scala
@@ -1,5 +1,50 @@
 package ucesoft.c64.peripheral.drive
 
+import java.io.ObjectOutputStream
+import java.io.ObjectInputStream
+import java.io.DataInputStream
+import java.io.FileInputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.DataOutputStream
+import java.io.IOException
+import ucesoft.c64.formats.D64
+import ucesoft.c64.formats.G64
+
+object Floppy {
+  def load(in:ObjectInputStream) : Option[Floppy] = {
+    in.readBoolean match {
+      case true =>
+        val file = in.readObject.asInstanceOf[String]
+        val content = in.readObject.asInstanceOf[Array[Byte]]
+        val tmpFile = new File(new File(System.getProperty("java.io.tmpdir")),new File(file).getName)
+        tmpFile.deleteOnExit
+        val out = new DataOutputStream(new FileOutputStream(tmpFile))
+        out.write(content)
+        out.close
+        val fileName = file.toUpperCase
+        val floppy : Floppy = if (fileName.endsWith(".D64")) new D64(tmpFile.toString) 
+                              else 
+                              if (fileName.endsWith(".G64")) new G64(tmpFile.toString)
+                              else throw new IOException(s"Can't load floppy $file")
+        floppy.load(in)
+        Some(floppy)
+      case false =>
+        None
+    }    
+  }  
+  def save(out:ObjectOutputStream,floppy:Option[Floppy]) {
+    floppy match {
+      case Some(f) =>
+        out.writeBoolean(true)
+        f.saveFile(out)
+        f.save(out)
+      case None =>
+        out.writeBoolean(false)
+    }    
+  }
+}
+
 /**
  * @author ealeame
  */
@@ -8,6 +53,7 @@ trait Floppy {
   val isReadOnly : Boolean
   val isFormattable : Boolean
   val totalTracks : Int
+  val file : String
   
   def nextBit : Int
   def writeNextBit(bit:Boolean)
@@ -29,12 +75,30 @@ trait Floppy {
   
   def close
   def reset
+  // state
+  def save(out:ObjectOutputStream)
+  def load(in:ObjectInputStream)
+  
+  protected def saveFile(out:ObjectOutputStream) {
+    val len = new File(file).length.toInt
+    val f = new DataInputStream(new FileInputStream(file))
+    try {
+      out.writeObject(file)
+      val content = Array.ofDim[Byte](len)
+      f.readFully(content)
+      out.writeObject(content)
+    }
+    finally {
+      f.close
+    }
+  }
 }
 
 object EmptyFloppy extends Floppy {
   val isReadOnly = false
   val isFormattable = false
   val totalTracks = 42
+  val file = ""
   private[this] var track = 1
   
   def nextBit = 0
@@ -57,5 +121,8 @@ object EmptyFloppy extends Floppy {
   def reset {
     track = 1
   }
+  // state
+  def save(out:ObjectOutputStream) {}
+  def load(in:ObjectInputStream) {}
   override def toString = "Empty Floppy"
 }
