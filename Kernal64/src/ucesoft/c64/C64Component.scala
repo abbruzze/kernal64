d4d55bd18360b599f6eec1e692f7df36e6dbe0be
ealeame <ealeame@IT00040215.ericsson.se>
2016-09-23 18:12:37 +0200

    State saving modifications

--- Kernal64/src/ucesoft/c64/C64Component.scala
+++ Kernal64/src/ucesoft/c64/C64Component.scala
@@ -2,6 +2,10 @@ package ucesoft.c64
 
 import scala.collection.mutable.ListBuffer
 import java.util.Properties
+import java.io.ObjectOutputStream
+import java.io.ObjectInputStream
+import javax.swing.JFrame
+import java.io.IOException
 
 object C64ComponentType extends Enumeration {
   type Type = Value
@@ -66,5 +70,47 @@ trait C64Component {
       c.components foreach { c => print(c,ind + 1) }
     }
     print(this,0)
+  }
+  
+  protected def saveState(out:ObjectOutputStream)
+  protected def loadState(in:ObjectInputStream)
+  protected def allowsStateRestoring(parent:JFrame) : Boolean
+  
+  final def save(out:ObjectOutputStream) {
+    Log.info(s"Saving $componentID/$componentType's state ...")
+    out.writeObject(componentID)
+    saveState(out)
+    for(c <- _components) c.save(out)    
+  }
+  final def load(in:ObjectInputStream) {
+    Log.info(s"Loading $componentID/$componentType's state ...")
+    val id = in.readObject.asInstanceOf[String]
+    if (id != componentID) throw new IOException(s"ID Mismatch: found $id, expected $componentID")
+    loadState(in)
+    for(c <- _components) c.load(in)
+  }
+  final def allowsState(parent:JFrame) : Boolean = {
+    if (allowsStateRestoring(parent)) {
+      _components forall { _.allowsStateRestoring(parent) }
+    }
+    else false
+  }
+  protected def saveClockEvents(out:ObjectOutputStream) {
+    val events = Clock.systemClock.getSubIdListFor(componentID)
+    out.writeObject(events)
+    for(e <- events) Log.info(s"Saving event ${e._1} for cycle ${e._2} for $componentID")
+  }
+  protected def loadClockEvents(in:ObjectInputStream)(f:Function2[Int,Long,ClockEvent]) {
+    val clk = Clock.systemClock
+    val events = in.readObject.asInstanceOf[List[(Int,Long)]]
+    for(e <- events) {
+      Log.info(s"Loading event ${e._1} for cycle ${e._2} for $componentID")
+      clk.schedule(f(e._1,e._2))
+    }
+  }
+  protected def loadMemory[T](mem:Array[T],in:ObjectInputStream) {
+    val ram = in.readObject.asInstanceOf[Array[T]]
+    if (ram.length != mem.length) throw new IOException(s"ROM/RAM length mismatch while loading $componentID.Expected ${mem.length}, found ${ram.length}")
+    Array.copy(ram,0,mem,0,ram.length)
   }
 }
