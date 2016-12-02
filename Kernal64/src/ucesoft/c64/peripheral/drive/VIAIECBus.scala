8b5dc7cc1899c9dfbd51156984121fb4245524df
abbruzze <alessandro.ametrano.abbruzzetti@ericsson.com>
2016-12-02 11:12:26 +0100

    Minor improvements looking at the electrical scheme

--- Kernal64/src/ucesoft/c64/peripheral/drive/VIAIECBus.scala
+++ Kernal64/src/ucesoft/c64/peripheral/drive/VIAIECBus.scala
@@ -15,7 +15,7 @@ class VIAIECBus(driveID:Int,
   override lazy val componentID = "VIA1 (Bus)"
   val busid = name
   private[this] val IDJACK = driveID & 0x03
-  private[this] var data_out = IECBus.VOLTAGE
+  //private[this] var data_out = IECBus.VOLTAGE
   private[this] var driveEnabled = true
   
   bus.registerListener(this)
@@ -23,7 +23,7 @@ class VIAIECBus(driveID:Int,
   
   override def reset {
     super.reset
-    data_out = IECBus.VOLTAGE
+    //data_out = IECBus.VOLTAGE
   }
   
   def setEnabled(enabled:Boolean) = driveEnabled = enabled
@@ -40,7 +40,7 @@ class VIAIECBus(driveID:Int,
   
   private def onCB1 = irq_set(IRQ_CB1)
   
-  override def read(address: Int, chipID: ChipID.ID) = address - startAddress match {
+  override def read(address: Int, chipID: ChipID.ID) = (address & 0x0F) match {
     case ad@(PA|PA2) =>
       super.read(address,chipID)
       if (driveID == 0 && ParallelCable.enabled) {
@@ -50,22 +50,17 @@ class VIAIECBus(driveID:Int,
       }
       else 0xFF
     case PB =>
-      (super.read(address,chipID) & 0x1A) | bus.data | bus.clk << 2 | bus.atn << 7 | IDJACK << 5
+      (super.read(address,chipID) & 0x1A) | (bus.data|data_out) | (bus.clk|clock_out) << 2 | bus.atn << 7 | IDJACK << 5
       
     case ofs => super.read(address,chipID)
   }
   
   override def write(address: Int, value: Int, chipID: ChipID.ID) {
     super.write(address,value,chipID)
-    address - startAddress match {
+    (address & 0x0F) match {
       case PB|DDRB =>        
-        val busValue = ~regs(DDRB) | regs(PB)
-        //data_out = if ((regs(DDRB) & 0x02) > 0 && (regs(PB) & 0x02) > 0) IECBus.GROUND else IECBus.VOLTAGE
-        data_out = if ((busValue & 0x02) > 0) IECBus.GROUND else IECBus.VOLTAGE
         bus.setLine(busid,IECBusLine.DATA,data_out)
-        //val clock_out = (regs(DDRB) & 0x08) > 0 && (regs(PB) & 0x08) > 0
-        val clock_out = (busValue & 0x08) > 0
-        bus.setLine(busid,IECBusLine.CLK,if (clock_out) IECBus.GROUND else IECBus.VOLTAGE)
+        bus.setLine(busid,IECBusLine.CLK,clock_out)
         
         autoacknwoledgeData
       case ad@(PA|PA2) =>
@@ -77,21 +72,23 @@ class VIAIECBus(driveID:Int,
     }         
   }
   
+  @inline private def data_out = if ((regs(DDRB) & regs(PB) & 0x02) > 0) IECBus.GROUND else IECBus.VOLTAGE
+  @inline private def clock_out = if ((regs(DDRB) & regs(PB) & 0x08) > 0) IECBus.GROUND else IECBus.VOLTAGE
+  
   @inline private def autoacknwoledgeData {
-    //val atna = (regs(DDRB) & 0x10) > 0 && (regs(PB) & 0x10) > 0
-    val atna = ((~regs(DDRB) | regs(PB)) & 0x10) > 0
+    val atna = (regs(DDRB) & regs(PB) & 0x10) > 0
+    //val atna = ((~regs(DDRB) | regs(PB)) & 0x10) > 0
     val dataOut = (bus.atn == IECBus.GROUND) ^ atna
     if (dataOut) bus.setLine(busid,IECBusLine.DATA,IECBus.GROUND) else bus.setLine(busid,IECBusLine.DATA,data_out)
   }
   // state
   override protected def saveState(out:ObjectOutputStream) {
     super.saveState(out)
-    out.writeInt(data_out)
+    //out.writeInt(data_out)
     out.writeBoolean(driveEnabled)
   }
   override protected def loadState(in:ObjectInputStream) {
     super.loadState(in)
-    data_out = in.readInt
     driveEnabled = in.readBoolean
   }
 }
