94c538b33693bf723b2eaa834667ae0957406333
Alessandro Abbruzzetti <abbruzze@gmail.com>
2018-07-23 17:11:41 +0200

    Removed unused library. Changed comment on external roms

--- Kernal64/build/k128.sh
+++ Kernal64/build/k128.sh
@@ -4,13 +4,13 @@ HOME=`dirname "$0"`
 HOME=`cd "$HOME"; pwd -P`
 LIB=$HOME/lib
 ROMS=$HOME/roms
-CP=$ROMS:$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar:$LIB/scala-parser-combinators_2.12-1.0.5.jar:$LIB/commons-net-3.3.jar:$LIB/dropbox-core-sdk-1.7.7.jar:$LIB/jackson-core-2.2.4.jar
+CP=$ROMS:$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar:$LIB/scala-parser-combinators_2.12-1.0.5.jar:$LIB/commons-net-3.3.jar
 if [ ! -x $JAVA_HOME/bin/java ]; then
         JAVA=java
 else
         JAVA=$JAVA_HOME/bin/java
 fi
-# to add custom Kernals set the variable below adding inside quotes -Dkernal=<kernal file> -D1541kernal=<1541 kernal file>
+# to add custom Kernals set the variable below adding -Dkernal64=<kernal 64 file> -Dkernal=<kernal 128 file> -D1541kernal=<1541 kernal file>
 # both kernal files must be placed under roms directory
 # example: KERNALS_ROM="-Dkernal=jiffydos_kernal.bin -D1541kernal=jiffydos1541_kernal.bin"
 KERNALS_ROMS=""
--- Kernal64/build/k64.sh
+++ Kernal64/build/k64.sh
@@ -4,7 +4,7 @@ HOME=`dirname "$0"`
 HOME=`cd "$HOME"; pwd -P`
 LIB=$HOME/lib
 ROMS=$HOME/roms
-CP=$ROMS:$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar:$LIB/scala-parser-combinators_2.12-1.0.5.jar:$LIB/commons-net-3.3.jar:$LIB/dropbox-core-sdk-1.7.7.jar:$LIB/jackson-core-2.2.4.jar
+CP=$ROMS:$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar:$LIB/scala-parser-combinators_2.12-1.0.5.jar:$LIB/commons-net-3.3.jar
 if [ ! -x $JAVA_HOME/bin/java ]; then
         JAVA=java
 else
