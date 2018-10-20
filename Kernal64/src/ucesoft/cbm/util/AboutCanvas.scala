ab5516f38a7a35d9f89b9c313379dad2fe929f03
abbruzze <abbruzze@gmail.com>
2018-10-20 19:33:55 +0200

    Added Scala version

--- Kernal64/src/ucesoft/cbm/util/AboutCanvas.scala
+++ Kernal64/src/ucesoft/cbm/util/AboutCanvas.scala
@@ -1,9 +1,26 @@
 package ucesoft.cbm.util
 
 import ucesoft.cbm.cpu.Memory
+import java.util.Properties
+import java.io.IOException
 
 class AboutCanvas(charRom:Memory,version:String) extends CBMCanvas(charRom) {
   private val WIDTH = 50
+  private val scalaVersion = {
+    val p = new Properties
+    Option(ClassLoader.getSystemResourceAsStream("library.properties")) match {
+      case None => "N/A"
+      case Some(in) =>
+        try {
+          p.load(in)
+          in.close
+          p.getProperty("version.number")
+        }
+        catch {
+          case _:IOException => "N/A"
+        }
+    }
+  }
   
   for(_ <- 1 to 4) newLine
   red
@@ -12,6 +29,7 @@ class AboutCanvas(charRom:Memory,version:String) extends CBMCanvas(charRom) {
   standardWidth
   add(center("VER " + version,WIDTH)).newLine
   add(center("JVM VERSION " + scala.util.Properties.javaVersion.replaceAll("_","-"),WIDTH)).newLine
+  add(center("SCALA VERSION " + scalaVersion,WIDTH)).newLine
   newLine
   white
   add(center("A COMMODORE 64/128 EMULATOR WRITTEN IN SCALA",WIDTH)).newLine
