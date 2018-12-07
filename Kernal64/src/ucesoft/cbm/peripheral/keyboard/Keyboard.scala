046531dfb54a622998b8d4969383534da97469dd
abbruzze <abbruzze@gmail.com>
2018-12-07 17:11:27 +0100

    Now the keyboard layout can be changed and the user can take effects immediately without restart.

--- Kernal64/src/ucesoft/cbm/peripheral/keyboard/Keyboard.scala
+++ Kernal64/src/ucesoft/cbm/peripheral/keyboard/Keyboard.scala
@@ -2,28 +2,74 @@ package ucesoft.cbm.peripheral.keyboard
 
 import java.awt.event.KeyListener
 import java.awt.event.KeyEvent
+
 import CKey._
 import ucesoft.cbm.CBMComponent
 import ucesoft.cbm.CBMComponentType
 import java.io.ObjectOutputStream
 import java.io.ObjectInputStream
-import javax.swing.JFrame
 
-class Keyboard(keyMapper: KeyboardMapper, nmiAction: (Boolean) => Unit = x => {},c128 : Boolean = false) extends KeyListener with CBMComponent {
+import javax.swing.JFrame
+import ucesoft.cbm.cpu.Memory
+
+object Keyboard {
+  def insertSmallTextIntoKeyboardBuffer(txt:String,mem:Memory,c64Mode:Boolean) {
+    val bufferAddr = if (c64Mode) 631 else 842
+    val lenAddr = if (c64Mode) 198 else 208
+    for(i <- 0 until txt.length) {
+      mem.write(bufferAddr + i,txt.charAt(i))
+    }
+    mem.write(lenAddr,txt.length)
+  }
+
+  def insertTextIntoKeyboardBuffer(txt:String,mem:Memory,c64Mode:Boolean): Unit = {
+    new Thread {
+      val maxLenAddr = if (c64Mode) 649 else 2592
+      val bufferAddr = if (c64Mode) 631 else 842
+      val lenAddr = if (c64Mode) 198 else 208
+      override def run {
+        val len = mem.read(maxLenAddr)
+        var strpos = 0
+        while (strpos < txt.length) {
+          val size = if (len < txt.length - strpos) len else txt.length - strpos
+          for(i <- 0 until size) {
+            val c = txt.charAt(strpos).toUpper
+            mem.write(bufferAddr + i,if (c != '\n') c else 0x0D)
+            strpos += 1
+          }
+          mem.write(lenAddr,size)
+          val ts = System.currentTimeMillis
+          // wait max 10 secs...
+          while (mem.read(lenAddr) > 0 && System.currentTimeMillis - ts < 10000) Thread.sleep(1)
+        }
+      }
+    }.start
+  }
+}
+
+class Keyboard(private var keyMapper: KeyboardMapper, nmiAction: (Boolean) => Unit = x => {},c128 : Boolean = false) extends KeyListener with CBMComponent {
   val componentID = "Keyboard"
   val componentType = CBMComponentType.INPUT_DEVICE 
   
   private[this] val keysPressed = collection.mutable.Set.empty[CKey.Key]
-  private[this] val keyMap = keyMapper.map
-  private[this] val keyPadMap = keyMapper.keypad_map
+  private[this] var keyMap = keyMapper.map
+  private[this] var keyPadMap = keyMapper.keypad_map
   private[this] val rowSelector,c128ExtendedRowSelector = Array.fill(8)(false)
   private[this] val colSelector = Array.fill(8)(false)
   private[this] var enabled,keypadEnabled = true
   // 128 CAPS-LOCK & 40/80 keys handling
   private[this] var c128_CapsLockPressed = false
   private[this] var c128_40_80_Pressed = false
-  private[this] val _40_80_KEY : Int = keyMapper.map.map(kv => (kv._2,kv._1)).getOrElse(_40_80,KeyEvent.VK_F9)
-  private[this] val CAPS_LOCK_KEY : Int = keyMapper.map.map(kv => (kv._2,kv._1)).getOrElse(CAPS_LOCK,KeyEvent.VK_CAPS_LOCK)
+  private[this] var _40_80_KEY : Int = keyMapper.map.map(kv => (kv._2,kv._1)).getOrElse(_40_80,KeyEvent.VK_F9)
+  private[this] var CAPS_LOCK_KEY : Int = keyMapper.map.map(kv => (kv._2,kv._1)).getOrElse(CAPS_LOCK,KeyEvent.VK_CAPS_LOCK)
+
+  def setKeyboardMapper(km:KeyboardMapper): Unit = {
+    keyMapper = km
+    keyMap = keyMapper.map
+    keyPadMap = keyMapper.keypad_map
+    _40_80_KEY = keyMapper.map.map(kv => (kv._2,kv._1)).getOrElse(_40_80,KeyEvent.VK_F9)
+    CAPS_LOCK_KEY = keyMapper.map.map(kv => (kv._2,kv._1)).getOrElse(CAPS_LOCK,KeyEvent.VK_CAPS_LOCK)
+  }
   
   def isCapsLockPressed = c128_CapsLockPressed
   def is4080Pressed = c128_40_80_Pressed
