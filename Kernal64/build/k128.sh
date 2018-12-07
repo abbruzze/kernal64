13d3294e6989e4150f233f4727bfc3337de08559
abbruzze <abbruzze@gmail.com>
2018-12-07 16:51:47 +0100

    Start scripts: removed old ROM handling via env variable.

--- Kernal64/build/k128.sh
+++ Kernal64/build/k128.sh
@@ -3,15 +3,10 @@
 HOME=`dirname "$0"`
 HOME=`cd "$HOME"; pwd -P`
 LIB=$HOME/lib
-ROMS=$HOME/roms
-CP=$ROMS:$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar:$LIB/scala-parser-combinators_2.12-1.0.5.jar:$LIB/commons-net-3.3.jar
+CP=$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar:$LIB/scala-parser-combinators_2.12-1.0.5.jar:$LIB/commons-net-3.3.jar
 if [ ! -x $JAVA_HOME/bin/java ]; then
         JAVA=java
 else
         JAVA=$JAVA_HOME/bin/java
 fi
-# to add custom Kernals set the variable below adding -Dkernal64=<kernal 64 file> -Dkernal=<kernal 128 file> -D1541kernal=<1541 kernal file>
-# both kernal files must be placed under roms directory
-# example: KERNALS_ROM="-Dkernal=jiffydos_kernal.bin -D1541kernal=jiffydos1541_kernal.bin"
-KERNALS_ROMS=""
-$JAVA -server -Xms64M -Xmx128M -cp $CP -Djava.library.path=$LIB $KERNALS_ROMS ucesoft.cbm.c128.C128 $*
+$JAVA -server -Xms64M -Xmx128M -cp $CP -Djava.library.path=$LIB ucesoft.cbm.c128.C128 $*
--- Kernal64/build/k64s.bat
+++ Kernal64/build/k64s.bat
@@ -1,10 +1,5 @@
 @echo off
 set HOME=%~dp0
 set LIB=%HOME%lib
-set ROMS=%HOME%roms
-set CP=%ROMS%;%LIB%\kernal64.jar;%LIB%\jinput.jar;%LIB%\scala-library.jar;%LIB%\scala-parser-combinators_2.12-1.0.5.jar;%LIB%\commons-net-3.3.jar
-rem to add custom Kernals set the variable below adding -Dkernal=<kernal file> -D1541kernal=<1541 kernal file>
-rem both kernal files must be placed under roms directory
-rem example: KERNALS_ROM=-Dkernal=jiffydos_kernal.bin -D1541kernal=jiffydos1541_kernal.bin
-set KERNALS_ROMS=
-java -server -Xms64M -Xmx128M -cp %CP% -Djava.library.path=%LIB% %KERNALS_ROMS% ucesoft.cbm.c64.C64 %*
+set CP=%LIB%\kernal64.jar;%LIB%\jinput.jar;%LIB%\scala-library.jar;%LIB%\scala-parser-combinators_2.12-1.0.5.jar;%LIB%\commons-net-3.3.jar
+java -server -Xms64M -Xmx128M -cp %CP% -Djava.library.path=%LIB% ucesoft.cbm.c64.C64 %*
