08a81f93df099853be2841945d866b1b691d7296
abbruzze <abbruzze@gmail.com>
2019-06-14 19:46:34 +0200

    scala combinators library updated for 2.13

--- Kernal64/build/k64.sh
+++ Kernal64/build/k64.sh
@@ -3,7 +3,7 @@
 HOME=`dirname "$0"`
 HOME=`cd "$HOME"; pwd -P`
 LIB=$HOME/lib
-CP=$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar:$LIB/scala-parser-combinators_2.12-1.0.5.jar:$LIB/commons-net-3.3.jar
+CP=$LIB/kernal64.jar:$LIB/jinput.jar:$LIB/scala-library.jar:$LIB/scala-parser-combinators_2.13-1.1.2.jar:$LIB/commons-net-3.3.jar
 if [ ! -x $JAVA_HOME/bin/java ]; then
         JAVA=java
 else
