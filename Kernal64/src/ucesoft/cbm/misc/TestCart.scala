0a9f8ac9688d06979da97cfe794f4c3e4328dcb7
abbruzze <abbruzze@gmail.com>
2021-01-16 21:00:19 +0100

    Fixed test cart for new screeshot handler.

--- Kernal64/src/ucesoft/cbm/misc/TestCart.scala
+++ Kernal64/src/ucesoft/cbm/misc/TestCart.scala
@@ -4,7 +4,7 @@ object TestCart {
   private[this] final val EXITCODE_LOCATION = 0xD7FF
   var enabled = false
   var screenshotFile : Option[String] = None
-  var screeshotHandler : java.io.File => Unit = _
+  var screeshotHandler : (java.io.File,() => Unit) => Unit = _
 
   final def write(address:Int,value:Int) : Unit = {
     if (enabled && address == EXITCODE_LOCATION) exit(value)
@@ -13,9 +13,9 @@ object TestCart {
   final def exit(value:Int): Unit = {
     screenshotFile match {
       case None =>
+        sys.exit(value)
       case Some(file) =>
-        screeshotHandler(new java.io.File(file))
+        screeshotHandler(new java.io.File(file),() => sys.exit(value))
     }
-    sys.exit(value)
   }
 }
