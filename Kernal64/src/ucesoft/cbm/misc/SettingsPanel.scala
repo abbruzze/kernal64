e4e0b0a423e358055d1d0e55315d9a4baa3abba0
Alessandro Abbruzzetti <abbruzze@gmail.com>
2021-03-05 15:00:48 +0100

    Table sets to not editable

--- Kernal64/src/ucesoft/cbm/misc/SettingsPanel.scala
+++ Kernal64/src/ucesoft/cbm/misc/SettingsPanel.scala
@@ -1,6 +1,7 @@
 package ucesoft.cbm.misc
 
 import java.awt.BorderLayout
+import javax.swing.table.DefaultTableModel
 import javax.swing.{JPanel, JScrollPane, JTable}
 
 class SettingsPanel(settings:Settings) extends JPanel {
@@ -11,7 +12,11 @@ class SettingsPanel(settings:Settings) extends JPanel {
     setLayout(new BorderLayout)
     val data : Array[Array[Object]] = settings.getSettings.sortBy(_.name) map { s => Array[Object](s"--${s.name}",s.description) } toArray
     val cols : Array[Object] = Array("Setting","Description")
-    val table = new JTable(data,cols)
+    val table = new JTable()
+    val model = new DefaultTableModel(data,cols) {
+      override def isCellEditable(row: Int, column: Int): Boolean = false
+    }
+    table.setModel(model)
     val sp = new JScrollPane(table)
     add("Center",sp)
   }
